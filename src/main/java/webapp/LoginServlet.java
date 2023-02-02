ac2add351f2c9562bb062540f8b90c75b5e7ebbf
kishorevjs <jayasaikishore@gmail.com>
2023-02-02 11:12:38 -0800

    Using doPost method to secure the parameters

--- src/main/java/webapp/LoginServlet.java
+++ src/main/java/webapp/LoginServlet.java
@@ -6,13 +6,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest httpServletRequest,
                          HttpServletResponse httpServletResponse) throws ServletException, IOException {
-        httpServletRequest.getRequestDispatcher("/WEB_INF/views/login.jsp").forward(httpServletRequest, httpServletResponse);
+
+        httpServletRequest.setAttribute("name", httpServletRequest.getParameter("name"));
+        httpServletRequest.setAttribute("password", httpServletRequest.getParameter("password"));
+
+        httpServletRequest.getRequestDispatcher("/WEB_INF/views/login.jsp").forward(httpServletRequest,
+                httpServletResponse);
+    }
+
+    protected void doPost(HttpServletRequest httpServletRequest,
+                          HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+        httpServletRequest.setAttribute("name", httpServletRequest.getParameter("name"));
+        httpServletRequest.getRequestDispatcher("/WEB_INF/views/welcome.jsp").forward(httpServletRequest,
+                httpServletResponse);
     }
 }
--- src/main/webapp/WEB_INF/views/login.jsp
+++ src/main/webapp/WEB_INF/views/login.jsp
@@ -1,9 +1,18 @@
+<%@ page import="java.util.Date" %>
 <!DOCTYPE html>
 <html>
 <head>
     <title>Hello...</title>
 </head>
 <body>
-My First JSP
+<%
+Date date = new Date();
+%>
+<div>Current date is <%=date%> </div>
+My First JSP ${name} and ${password}
+<form action="/login.do" method="post">
+    Enter your name <input type="text" name="name"/> <input type="submit"/>
+
+</form>
 </body>
 </html>
